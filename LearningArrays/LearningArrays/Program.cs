d2875d69611c17285fc53d9abfd4075d02b23308
RusneV <rusnevenclovaite@gmail.com>
2022-12-12 16:20:39 +0200

    Added "Learning Arrays" project

---
+++ LearningArrays/LearningArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an array of string size 5. Fill array items with values.
+            string[] characters = new string[5] { "Name1", "Name2", "Name3", "Name4", "Name5" };
+            
+            //Print all words using loop
+            foreach (string state in characters)
+            {
+                // The `state` variable takes on the value of an element in `states` and updates every iteration.
+                Console.WriteLine(state);
+            }
+
+            //Create an int array size 20. 
+            int[] intArray = new int[20];
+
+            // Fill values from 0 till 20.
+            for (int i = 0; i < 20; i++)
+            {
+                intArray[i] = i;
+            }
+
+            // Print numbers backwards
+            for (int i = intArray.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+        }
+    }
+}
